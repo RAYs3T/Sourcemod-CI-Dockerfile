e205cd5bf07ad6593c8462f1bcd1e3d2bba7f95a
root <root@lilly.ptl-clan.de>
2017-10-08 13:48:26 +0200

    Init files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Sourcemod-CI-Dockerfile
 A docker sourcemod plugins build environment, based on debian
+
+## Installation
+TODO
+
+## Use together with GitLab runner
+If you want to use this docker image for your sourcemod plugins, using GitLab, here some short notes how to.
+
+### .gitlab-ci.yml
+GitLab uses a .yml configuration file for the runner configuration. If you completly new to this, have a look at the [docs over at GitLab](https://docs.gitlab.com/ce/ci/examples/README.html).
+
+This docker images adds a little wrapper script for the script compile precedure, which is callable by simply running the spcom command (registered as symlink to the real script).
+The script is basically looking for *.sp files in the build directory and compiles those.
+
+So all you have to do in your .gitlab-ci.yml is *invoking the command "spcomp"* after the docker machine is started. This will then start the compilation of your plugins.
---
+++ compile.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Path the the compiler executable
+SP_COMP_EXE=/home/smuser/sourcemod/addons/sourcemod/scripting/spcomp
+
+# Working directory for custom plugins
+BUILD_DIR=$CI_PROJECT_DIR
+
+# Dir for compiled plugins
+COMPILE_DIR=$BUILD_DIR/compiled
+
+
+additional_params=$1
+echo "Using additional params: $additional_params"
+
+function compile_failed() {
+        echo "Seem like a script does not compile, check error out from spcomp above for more details"
+        exit 1;
+}
+
+# Change to the root of the sourcemod scripting directory so sourcemod can find all its include dependencies
+
+echo "Compiled smx files going to: $COMPILE_DIR"
+
+# Check if the build dir contains an include folder
+# THis is not required, but the user should see a message, if it is missing
+if [ ! -d $BUILD_DIR/includes ]; then
+	echo "WARNING: Your build directory ($BUILD_DIR) does not contain an includes folder.\n\
+This means if your plugin has non-standard (not included in sourcemod) dependencies, these cannot be loaded"
+fi
+
+test -e $COMPILE_DIR || mkdir $COMPILE_DIR
+
+dir_before=$(pwd)
+cd $BUILD_DIR
+for sourcefile in *.sp
+do
+	# Guard to prevent processing *.sp when no files in directory
+	if [ ! -f "$sourcefile" ]; then
+		echo "No files found for compilation"
+		break;
+	fi
+	smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
+	echo -e "Compiling $sourcefile ..."
+	$SP_COMP_EXE $sourcefile -o$COMPILE_DIR/$smxfile $additional_params
+	RETVAL=$?
+	if [ $RETVAL -ne 0 ]; then
+		compile_failed
+	fi
+done
+cd $dir_before
+
